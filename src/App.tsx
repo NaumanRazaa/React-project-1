ecc023544add43304ec81be83ed48d7da5ddc806
NaumanRazaa <maliknaumanraza674@gmail.com>
2026-09-18 17:17:18 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { APITester } from "./APITester";
+import { Project1 } from "./Components/Project1";
+import { Sample } from "./Components/Sample";;
+
+import logo from "./logo.svg";
+import reactLogo from "./react.svg";
+
+export function App() {
+  return (
+    <div>
+  <Project1/>
+      
+     
+    </div>
+    
+  );
+}
+
+export default App;
